63ccedf319e722e93d580daa55f76e8a4d6b1744
shovan777 <raj.shrestha778@gmail.com>
2025-02-12 16:18:04 -0500

    Ignore non dev files.

--- basic_kf.py
+++ basic_kf.py
@@ -1,16 +1,10 @@
 """Module to create the basic structure of kalman filter."""
 import numpy as np
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from collections import OrderedDict
 
 @dataclass
-class PhysicalModel():
-    constants: dict = {}
-    state: dict = OrderedDict() # contains all the state variables, X
-    input: dict = OrderedDict() # contains  all the input variables, U
-    measurement: dict = OrderedDict() # contains all output variables
-    # also known as measurements Z or Y
-    # this also represents the order of the state variables
+class PhysicalModel:
     state_v: np.ndarray # state vector, X
     input_v: np.ndarray # model inputs or input excitations
     measurement_v: np.ndarray # sensor measurement vector, Z
@@ -19,8 +13,16 @@ class PhysicalModel():
     input_mat: np.ndarray # input matrix B
     output_mat: np.ndarray # output matrix D if inputs influence measurements
     process_error_cov_mat: np.ndarray # process error covariance matrix, P
-    meas_noise_cov_mat: np.ndarray # measurement noise covariance matrix, Q
-    process_noise_cov_mat: np.ndarray # process noise covariance matrix, R
+    meas_noise_cov_mat: np.ndarray # measurement noise covariance matrix, R
+    process_noise_cov_mat: np.ndarray # process noise covariance matrix, Q
+    # variables with default values
+    constants: dict = field(default_factory=OrderedDict)# contains all the constants
+    state: dict = field(default_factory=OrderedDict)# contains all the state variables, X
+    input: dict = field(default_factory=OrderedDict)# contains  all the input variables, U
+    measurement: dict = field(default_factory=OrderedDict) # contains all output variables
+    # also known as measurements Z or Y
+    # this also represents the order of the state variables
+    
     
 
 def zero_initializer(mat_size: tuple) -> np.ndarray:
@@ -47,7 +49,7 @@ def identity_initializer(row_size: int) -> np.ndarray:
 
 
 class KalmanFilter:
-    def __init__(self, model: PhysicalModel, init_vals: dict):
+    def __init__(self, model: PhysicalModel, time_delta:float, init_vals: dict):
         """Initialize the Kalman filter.
         Load all the constants with values and also
         initialize the covariance matrices.
@@ -57,19 +59,21 @@ class KalmanFilter:
             init_vals (dict): dict containing all the constants.
         """
         self.model = model
-        self.delta_t = 0.01
-        for key in self.model.constants.keys:
-            try:
-                self.model[key] = init_vals[key]
-            except KeyError as ke:
-                raise "Initializer should have all model constants."
+        self.delta_t = time_delta
+        # TODO: remove initialization to model itself
+        # not necessary for KF to know about the model
+        # for key in self.model.constants.keys:
+        #     try:
+        #         self.model[key] = init_vals[key]
+        #     except KeyError as ke:
+        #         raise "Initializer should have all model constants."
     
     def predict_next_state(self, input_v: np.ndarray) -> np.ndarray:
-        next_state_priori_v = (
+        next_state_priori = (
             self.model.transition_mat @ self.model.state_v + 
-            self.model.input_mat @ self.model.input_v
+            self.model.input_mat @ input_v
         )
-        return next_state_priori_v
+        return next_state_priori
 
     def predict_measurement(self, input_v: np.ndarray) -> np.ndarray:
         # most of the time output_mat will be zero
@@ -77,14 +81,14 @@ class KalmanFilter:
         # in general
         measurement_estimate = (
             self.model.measurement_mat @ self.model.state_v +
-            self.model.output_mat @ self.model.input_v
+            self.model.output_mat @ input_v
         )
         return measurement_estimate
 
-    def estimate_process_error(self):
+    def estimate_error_cov(self):
         return (
             self.model.transition_mat @ self.model.process_error_cov_mat @ self.model.transition_mat.T +
-            self.model.meas_noise_cov_mat
+            self.model.process_noise_cov_mat
         )
 
     def calculate_gain(self):
@@ -92,14 +96,13 @@ class KalmanFilter:
             (self.model.process_error_cov_mat @ self.model.measurement_mat.T) @
             np.linalg.inv(
                 ((self.model.measurement_mat @ self.model.process_error_cov_mat) @
-                self.model.measurement_mat.T) + self.model.process_noise_cov_mat
+                self.model.measurement_mat.T) + self.model.meas_noise_cov_mat
             )
         )
     
-    def calculate_innovation(self, measurement_estimate):
-        return self.model.measurement_v - measurement_estimate
+    def calculate_innovation(self, actual_measurement: np.ndarray) -> np.ndarray:
+        return actual_measurement - self.model.measurement_v
     
-    @staticmethod
     def update_next_state(
             self,
             innovation_v: np.ndarray, 
@@ -108,26 +111,30 @@ class KalmanFilter:
             ):
         self.model.state_v = next_state_priori_v + kalman_gain @ innovation_v
 
-    def updade_err_cov_mat(self, estimated_process_noise: np.ndarray, kalman_gain: np.ndarray):
-        self.model.process_error_cov_mat = (np.eye(len(self.model.state_v)) - kalman_gain @ self.model.meas_noise_cov_mat) @ estimated_process_noise
+    def updade_err_cov_mat(self, estimated_error_cov: np.ndarray, kalman_gain: np.ndarray):
+        self.model.process_error_cov_mat = (np.eye(len(self.model.state_v)) - kalman_gain @ self.model.measurement_mat) @ estimated_error_cov
 
-    def kalman_step(self, input_v: np.ndarray):
+    def kalman_step(self, input_v: np.ndarray, actual_measurement: np.ndarray):
         # prediction step
         # x = Fx + Bu
-        next_state_priori_v = self.predict_next_state(input_v)
+        self.model.state_v = self.predict_next_state(input_v)
 
         # z = Hx + Du
-        measurement_estimate = self.predict_measurement(input_v)
+        self.model.measurement_v = self.predict_measurement(input_v)
         
         # P = FPF^T + Q
-        estimated_process_noise = self.estimate_process_error()
+        estimated_process_noise = self.estimate_error_cov()
 
         # update step
         # K = PH^T(HPH^T + R)^-1
         kalman_gain = self.calculate_gain()
+
+        # innovation/error
         # y_err = z - Hx
-        innovation_v = self.calculate_innovation(measurement_estimate)
+        innovation_v = self.calculate_innovation(actual_measurement)
+
         # x = x + K*y_err
-        self.update_next_state(innovation_v, next_state_priori_v, kalman_gain)
+        self.update_next_state(innovation_v, self.model.state_v, kalman_gain)
+
         # P = (I - KH)P
         self.updade_err_cov_mat(estimated_process_noise, kalman_gain)
